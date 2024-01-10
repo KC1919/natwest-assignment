536c209471d2339e040e31f5cdc791d3dedccc90
Kunal <kunal.chandra1900@gmail.com>
2024-01-11 01:16:18 +0530

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ EC2_INSTANCE_IP="$1"
 #
 SSH_KEY="$2"
 # Update package lists and install Nginx
-ssh -i $SSH_KEY ec2-user@$EC2_INSTANCE_IP 'sudo yum update -y && sudo amazon-linux-extras install nginx1.12 -y && sudo systemctl start nginx && sudo systemctl enable nginx'
+ssh -i $SSH_KEY ec2-user@$EC2_INSTANCE_IP 'sudo yum update -y && sudo amazon-linux-extras install httpd -y && sudo systemctl start httpd && sudo systemctl enable httpd'
 
 # Deploy HTML page
 ssh -i $SSH_KEY ec2-user@$EC2_INSTANCE_IP 'echo "<html><head><title>Deployed by Script</title></head><body><h1>Hello from EC2!</h1></body></html>" | sudo tee /var/www/html/index.html'
